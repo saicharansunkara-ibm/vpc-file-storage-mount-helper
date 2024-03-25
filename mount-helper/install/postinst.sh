2745249a12353a69e1027e9a20c79b5402a10ecc
Prankul <84079249+prankulmahajan@users.noreply.github.com>
2024-03-25 18:45:45 +0530

    revert IKS make targets
    Signed-off-by: Prankul <84079249+prankulmahajan@users.noreply.github.com>

--- mount-helper/install/postinst.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SBIN_SCRIPT="/sbin/mount.ibmshare"
-CERT_PATH="/tmp/metadata"
-
-cp /tmp/charon-log.conf /etc/strongswan.d/
-
-$SBIN_SCRIPT -INSTALL_ROOT_CERT $CERT_PATH
--- mount-helper/install/prerm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SCRIPT_NAME="mount.ibmshare"
-SBIN_SCRIPT="/sbin/$SCRIPT_NAME"
-
-$SBIN_SCRIPT -TEARDOWN_APP
